32de550206d72c45f27af8f75a3eea29a5c8be0b
monsoonjunkie <anthonyle925@gmail.com>
2019-07-25 16:39:59 -0700

    finished drills01

--- drills01/yourcode.js
+++ drills01/yourcode.js
@@ -1,16 +1,41 @@
 
-function sumArray(){
-	
+function sumArray(array){
+    var sum = 0;
+    for(var i = 0; i < array.length; i++){
+        sum = sum + array[i];
+    }
+    return sum;
+    
+    
 }
 
-function fitWithinVal(){
+function fitWithinVal(array, total){
+    var newArray = [];
+    var sum = null;
+    var i = 0;
+    while(i < array.length){
+        if(sum + array[i] <= total){
+            sum = sum + array[i];
+            newArray.push(array[i]);
+            i++
+        }else{
+            i++;
+        }
+    }
+    return newArray;
 
 }
 
-function getAllNamesShorterThan(){
-
+function getAllNamesShorterThan(array, lengthVal){
+    var newArray = [];
+    for(var i = 0; i < array.length; i++){
+        if(array[i].length <= lengthVal){
+            newArray.push(array[i]);
+        }
+    }
+    return newArray;
 }
 
-function makeLabel(){
-
+function makeLabel(label){
+    return ""+label.greeting+" "+label.givenName+" "+label.familyName+"\n"+label['home address'].streetNumber+" "+label['home address'].streetName+"\n"+label['home address'].city+","+" "+label['home address'].state+" "+label['home address'].zip;
 }
