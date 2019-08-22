b45b8e752c5dd9a8b78569a8ecc7d22c7027a9fb
monsoonjunkie <anthonyle925@gmail.com>
2019-08-21 18:21:45 -0700

    drill04 wip

--- drills04/yourcode.js
+++ drills04/yourcode.js
@@ -1,7 +1,38 @@
 
 
-function changeElements(){
-
+function changeElements(className){
+    
+    let convert = $(className).text();
+    console.log(convert);
+    if( convert === 'one'){
+        return convert = 1
+    }
+    if( convert === 'two'){
+        return convert = 2;
+    }
+    if( convert === 'three'){
+        return convert = 3;
+    }
+    if( convert === 'four'){
+        return convert = 4;
+    }
+    if( convert === 'five'){
+        return convert = 5;
+    }
+    if( convert === 'six'){
+        return convert = 6;
+    }
+    if( convert === 'seven'){
+        return convert = 7;
+    }
+    if( convert === 'eight'){
+        return convert = 8;
+    }
+    if( convert === 'nine'){
+        return convert = 9;
+    }
+    return convert;
+    
 }
 
 function appendTextToElement(){
