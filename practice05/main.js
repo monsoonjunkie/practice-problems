6bfe255fa698b9686bb27043e1dd7435b1eaf068
monsoonjunkie <anthonyle925@gmail.com>
2019-08-22 19:45:30 -0700

    Completed fizzBuzz function

--- practice05/main.js
+++ practice05/main.js
@@ -1,19 +1,17 @@
 function fizzBuzz(){
-    // let number = Math.floor(Math.random()*100) + 1;
-    let number = 15;
-    debugger;
+    let number = Math.floor(Math.random()*100) + 1;
+
     if(number % 3 === 0 && number % 5 === 0){
         return console.log('FizzBuzz');
-    }
-    else if(number % 3 === 0){
+    
+    } else if(number % 3 === 0){
         return console.log('Fizz');
-    }
-    else if (number % 5 === 0) {
+    
+    } else if (number % 5 === 0) {
         return console.log('Buzz');
-    }
-    else{
-        return number;
+    
+    } else {
+        return console.log(number);
     }
 
-    
 }
