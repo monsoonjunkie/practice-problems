66d21ac578ee8ee7dbc9ea25062db43bcea7b7a8
monsoonjunkie <anthonyle925@gmail.com>
2019-08-22 17:37:19 -0700

    Completed practice2 reverse array fixed iteration

--- practice02/main.js
+++ practice02/main.js
@@ -1,6 +1,6 @@
 function arrayReverse (arr) {
     let newArr = [];
-    for(let i = arr.length; i >= 0; i--){
+    for(let i = arr.length - 1; i >= 0; i--){
         let last = arr.pop();
         newArr.push(last);
     }
