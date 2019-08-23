111871cb2a08b6f1d7f6ae596227b546bb842ba3
monsoonjunkie <anthonyle925@gmail.com>
2019-08-22 18:05:11 -0700

    remove pop and clean solution more

--- practice02/main.js
+++ practice02/main.js
@@ -1,7 +1,6 @@
 function arrayReverse (arr) {
     let newArr = [];
     for(let i = arr.length - 1; i >= 0; i--){
-        let last = arr.pop();
         newArr.push(last);
     }
     return newArr;
